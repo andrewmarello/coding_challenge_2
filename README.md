66cfd1253cdea9317384201c08a91e461bdfc6dc
Andrew <andrewmarello@usf.edu>
2026-01-29 16:22:36 -0500

    Updated files for README.md, index.html, and js changes.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Coding Challenge 2
 
+ISM3232 Challenge 2a and 2b.
--- challenge_2a.js
+++ challenge_2a.js
@@ -1,3 +1,6 @@
 
 const name = "Andrew"
 console.log(`Hello ${name}.`)
+
+let number = 22
+console.log(`Your favorite number is ${number}.`)
